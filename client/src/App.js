68915120cea18374710beb316cc418203d687497
koolname99 <72174759+koolname99@users.noreply.github.com>
2023-09-29 14:13:52 -0500

    tutor edit profile
    2nd commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import profileImage from "./profile.jpg";
+import * as React from "react";
+import TutorEditProfile from "./pages/tutoredit";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-  // Generate arrays for hours, minutes, and AM/PM options
-  const hours = Array.from({ length: 12 }, (_, i) => (i + 1).toString());
-  const minutes = Array.from({ length: 60 }, (_, i) =>
-    i.toString().padStart(2, "0")
-  );
-  const amPmOptions = ["AM", "PM"];
-
   return (
     <div className="App">
-      <aside className="sidemenu">
-        <div className="side-menu-button">
-          <h1>Home</h1>
-          <h1>Calendar</h1>
-          <h1>Jonathan Nguyen</h1>
-          <div className="profile-container">
-            <img src={profileImage} alt="Profile" width="50" height="50" />
-          </div>
-        </div>
-      </aside>
-      <div className="container" style={{ backgroundColor: "white" }}>
-        <input type="text" placeholder="Bio" name="bio" required />
-        <input type="text" placeholder="Email" name="email" required />
-        <input type="text" placeholder="Password" name="password" required />
-        <input type="text" placeholder="Courses" name="courses" required />
-
-        {/* Availability input */}
-        <div className="availability-container">
-          <label htmlFor="availability">Availability:</label>
-          <div className="availability-inputs">
-            <select name="availability-hour">
-              {hours.map((hour) => (
-                <option key={hour} value={hour}>
-                  {hour}
-                </option>
-              ))}
-            </select>
-            <span>:</span>
-            <select name="availability-minute">
-              {minutes.map((minute) => (
-                <option key={minute} value={minute}>
-                  {minute}
-                </option>
-              ))}
-            </select>
-            <select name="availability-am-pm">
-              {amPmOptions.map((amPm) => (
-                <option key={amPm} value={amPm}>
-                  {amPm}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="container">
-          <input type="submit" value="Save changes" />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/tutoredit" element={<TutorEditProfile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/tutoredit.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import profileImage from '../profile.jpg';
+import "../App.css";
+
+const TutorEditProfile = () => {
+  // Generate arrays for hours, minutes, and AM/PM options
+  const hours = Array.from({ length: 12 }, (_, i) => (i + 1).toString());
+  const minutes = Array.from({ length: 60 }, (_, i) =>
+    i.toString().padStart(2, '0')
+  );
+  const amPmOptions = ['AM', 'PM'];
+
+  return (
+    <div>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <h1>Home</h1>
+          <h1>Calendar</h1>
+          <h1>Jonathan Nguyen</h1>
+          <div className="profile-container">
+            <img src={profileImage} alt="Profile" width="50" height="50" />
+          </div>
+        </div>
+      </aside>
+
+      {/* Bio input */}
+      <div className="input-container">
+        <label htmlFor="bio">Bio:</label>
+        <input type="text" placeholder="Bio" name="bio" required />
+      </div>
+
+      {/* Email input */}
+      <div className="input-container">
+        <label htmlFor="email">Email:</label>
+        <input type="text" placeholder="Email" name="email" required />
+      </div>
+
+      {/* Password input */}
+      <div className="input-container">
+        <label htmlFor="password">Password:</label>
+        <input type="text" placeholder="Password" name="password" required />
+      </div>
+
+      {/* Courses input */}
+      <div className="input-container">
+        <label htmlFor="courses">Courses:</label>
+        <input type="text" placeholder="Courses" name="courses" required />
+      </div>
+
+      {/* Availability input */}
+      <div className="availability-container">
+      <label htmlFor="availability">Availability:</label>
+        <div className="availability-inputs">
+          <label htmlFor="from-time">From: </label>
+          <select name="from-time-hour">
+            
+            {hours.map((hour) => (
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          <span>:</span>
+          <select name="from-time-minute">
+            
+            {minutes.map((minute) => (
+              <option key={minute} value={minute}>
+                {minute}
+              </option>
+            ))}
+          </select>
+          <select name="from-time-am-pm">
+            {amPmOptions.map((amPm) => (
+              <option key={amPm} value={amPm}>
+                {amPm}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="availability-inputs">
+          <label htmlFor="to-time">To: </label>
+          <select name="to-time-hour">
+            
+            {hours.map((hour) => (
+              <option key={hour} value={hour}>
+                {hour}
+              </option>
+            ))}
+          </select>
+          <span>:</span>
+          <select name="to-time-minute">
+            
+            {minutes.map((minute) => (
+              <option key={minute} value={minute}>
+                {minute}
+              </option>
+            ))}
+          </select>
+          <select name="to-time-am-pm">
+            {amPmOptions.map((amPm) => (
+              <option key={amPm} value={amPm}>
+                {amPm}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TutorEditProfile;
