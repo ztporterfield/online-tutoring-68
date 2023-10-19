4b21926cfc00d21c916d3660a20a8c1edc89e6c4
Sarem Hussein <snh220000@utdallas.edu>
2023-10-18 23:46:33 -0500

    Made changes to tutor profile,used api

--- client/src/pages/TutorProfile.js
+++ client/src/pages/TutorProfile.js
@@ -1,9 +1,41 @@
-import React from "react"
+import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
+import axios from 'axios'
 
 export default function TutorProfilePage() {
     let { id } = useParams()
-    const tutor = fetchTutor(id)
+    const [tutor, setTutor] = useState({
+        id: id,
+        first_name: "",
+        last_name: "",
+        bio: "",
+        profile_picture_url: "https://fastly.picsum.photos/id/1/200/200.jpg?hmac=jZB9EZ0Vtzq-BZSmo7JKBBKJLW46nntxq79VMkCiBG8",
+        subjects: [],
+        available_hours_start: "",
+        available_hours_end: ""
+    });
+    useEffect(() => {
+        axios.get(`http://localhost:8800/tutors/${id}`)
+            .then((response)=>{
+                let profile_picture_url = tutor.profile_picture_url;
+                if(response.data.ProfilePictureID) {
+                    profile_picture_url = `http://localhost:8800/images/${response.data.ProfilePictureID}`;
+                }
+                setTutor({
+                    ...tutor,
+                    id: response.data.ID,
+                    first_name: response.data.FirstName,
+                    last_name: response.data.LastName,
+                    bio: response.data.Bio,
+                    subjects: [response.data.Subject],
+                    available_hours_start: response.data.AvailableHoursStart,
+                    available_hours_end: response.data.AvailableHoursEnd,
+                    profile_picture_url: profile_picture_url
+                })
+            }).catch((error)=>{
+                alert(error.response.data.sqlMessage || error)
+            });
+    }, [])
     return (
         <div className="flex flex-col gap-2 bg-gray-100 rounded-lg py-8 px-10 shadow-lg">
             <img src={tutor.profile_picture_url} className="rounded-xl w-1/2" />
@@ -18,7 +50,7 @@ export default function TutorProfilePage() {
                     Subjects
                 </h2 >
                 <ul className="flex gap-2">
-                    {tutor.subjects.map((subject) => (<li className="bg-blue-600 text-white px-2 py-1 rounded-lg">{subject}</li>))}
+                    {tutor.subjects.map((subject) => (<li key={subject} className="bg-blue-600 text-white px-2 py-1 rounded-lg">{subject}</li>))}
                 </ul>
             </div>
             <div>
@@ -30,24 +62,10 @@ export default function TutorProfilePage() {
                 </h3>
             </div>
             <div>
-                <h2 className="text-xl font-bold">
-                    Bookings
-                </h2>
+                <a href="#" className="flex text-xl text-white justify-center bg-blue-500 hover:bg-blue-600 rounded-md p-2 w-full cursor-pointer">
+                    Show Bookings
+                </a>
             </div>
         </div>
     )
-}
-
-function fetchTutor(id) {
-    //TO DO: Change this to use the API
-    return {
-        id: id,
-        first_name: "Alex",
-        last_name: "Hat",
-        bio: "I am a maths tutor I am a maths tutor I am a maths tutor I am a maths tutor I am a maths tutor I am a maths tutor",
-        profile_picture_url: "https://fastly.picsum.photos/id/1/200/200.jpg?hmac=jZB9EZ0Vtzq-BZSmo7JKBBKJLW46nntxq79VMkCiBG8",
-        subjects: ["Biology", "Chemistry", "Maths"],
-        available_hours_start: "12:00",
-        available_hours_end: "3:00"
-    }
 }
